41e96c497fcd2f377446a3956c1ac15c67fe36aa
toremh <68994615+toremh@users.noreply.github.com>
2024-07-31 16:19:27 +1200

    Original Project

---
+++ Assets/ExitDetector.cs
@@ -0,0 +1,35 @@
+﻿/* Exit Detector by Ben Elwood
+* Made for use by design students in the "Escape Lockdown 2030" project
+* V2 published on 14/10/2020
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExitDetector : MonoBehaviour
+{
+
+    public UnityEvent OnExitSuccess = null;
+
+    public bool[] corrects;
+
+    public void addCorrect(int index)
+    {
+        bool everythingCorrect = true;
+        corrects[index] = true;
+        for (int i = 0; i < corrects.Length; i++)
+        {
+            if (corrects[i] == false)
+            {
+                everythingCorrect = false;
+            }
+        }
+        if (everythingCorrect)
+        {
+            OnExitSuccess.Invoke();
+        }
+    }
+
+
+}
---
+++ Assets/VR Beginner/Scripts/Tutorial/FuseBox.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseBox : MonoBehaviour
+{
+    public ParticleSystem[] SparkleFuseVFX;
+    public ParticleSystem[] SwitchedOnVFX;
+    public ParticleSystem[] SwitchedOffVFX;
+
+    public List<GameObject> lights;
+    
+    bool m_FusePresent = false;
+
+    public void Switched(int step)
+    {
+        if (!m_FusePresent)
+            return;
+
+        if (step == 0)
+        {
+            foreach (var s in SwitchedOffVFX)
+            {
+                s.Play();
+            }
+        }
+        else
+        {
+            foreach (var s in SwitchedOnVFX)
+            {
+                s.Play();
+            }
+        }
+    }
+    
+    public void FuseSocketed(bool socketed)
+    {
+        m_FusePresent = socketed;
+
+        if (m_FusePresent)
+        {
+            foreach (var s in SparkleFuseVFX)
+            {
+                s.Play();
+                foreach(GameObject light in lights)
+                {
+                    light.SetActive(true);
+                }
+            }
+        }
+        else
+        {
+            foreach (GameObject light in lights)
+            {
+                light.SetActive(false);
+            }
+        }
+    }
+}
